3a83969d09debcd975b064d50950458b05c5ee05
Jaiswal-Sneha <70856862+Jaiswal-Sneha@users.noreply.github.com>
2020-11-23 13:41:53 +0530

    prac8.java

--- prac8.java
+++ prac8.java
@@ -1,10 +1,18 @@
-public class argument {
-       public static void main(String [ ] args) 
-   {
-              int a= Integer. parseInt ( args[0]) ;
-              int b= Integer. parseInt ( args[1]) ;
-              int sum=a + b;
-
-       System. out. println("The Sum is " +sum) ;
-   }
+import java.util.*;
+public class string1 {
+    public static void main (String args[])
+    {
+        Scanner inp = new Scanner(System.in);
+		System.out.print("Enter string name: ");
+        String name = inp.nextLine();
+        System.out.println("Name: " + name);
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("First char: " + name.charAt(0));
+        System.out.println("Length: " + name.length());
+        
+        
+    }
 }
+
+
